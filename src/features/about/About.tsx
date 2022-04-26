4ca94851bab50df978a66016e0ec56a7fcf6c58e
Jack Bloomfeld <jackbloomfeld1@gmail.com>
2022-04-25 19:23:00 -0700

    Text updates

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -10,34 +10,42 @@ export function About() {
       </a>
       <div className="App-page">
         <header className="App-header">
-          <p className="subtitle-container">Welcome!<br></br>- the Team.</p>
+          <p className="subtitle-container">
+            Welcome!<br></br>- the Team.
+          </p>
           <div className="row">
-          <p className="FounderProfile">
-          <p className="FounderTitle">Troy, CEO & Co-founder</p>
-          Troy has spent the past three years in product at Microsoft 
-          where he has shipped products from natural language understanding SDK’s to AI powered Windows 
-          experiences. He has had the opportunity to ship features that are used by hundreds of millions 
-          of users today. <br></br> In the past year Troy has been investing in, learning about, and getting 
-          involved with the Web3 space and wants to work to bring the amazing promises of Web3 to the masses.
-          <br />
-          </p>
-          <p className="FounderProfile">
-          <p className="FounderTitle">Jack, CTO & Co-founder</p>
-          <br />
-          Jack has experience as a software engineer at Microsoft where he has
-          experience with building resilient microservice architecture that
-          served billions of requests. He has spent the past year on UX
-          development as well.<br></br>He has been an avid web3 researcher for a few
-          years and is active in many web3 communities.
-          <br />
-          </p>
-          <p className="FounderProfile">
-          <p className="FounderTitle">Jacob, CPO & Co-founder</p>
-          <br />
-          Jacob is a musician that has made several EPs and has performed in his own band.<br></br> 
-          Jacob has 4 years of experience as a professional audio engineer. He has worked on Emmy, 
-          Grammy, and Oscar winning projects and has toured in 4 continents with notable artists
-          </p>
+            <p className="FounderProfile">
+              <p className="FounderTitle">Troy, CEO & Co-founder</p>
+              Troy has spent the past three years in product at Microsoft where
+              he has shipped products from natural language understanding SDK’s
+              to AI powered Windows experiences. He has had the opportunity to
+              ship features that are used by hundreds of millions of users
+              today. <br></br> In the past year Troy has been investing in,
+              learning about, and getting involved with the Web3 space and wants
+              to work to bring the amazing promises of Web3 to the masses.
+              <br />
+            </p>
+            <p className="FounderProfile">
+              <p className="FounderTitle">Jack, CTO & Co-founder</p>
+              <br />
+              Jack has experience as a software engineer at Microsoft where he
+              worked on building resilient microservice architecture for the
+              commerce team that served billions of requests to customers. He
+              has spent the past year as a full stack engineer for the
+              experimentation (A/B testing) platform as well.<br></br>He has
+              been an avid web3 researcher for many years and is active in many
+              web3 communities.
+              <br />
+            </p>
+            <p className="FounderProfile">
+              <p className="FounderTitle">Jacob, CPO & Co-founder</p>
+              <br />
+              Jacob is a musician that has made several EPs and has performed in
+              his own band.<br></br>
+              Jacob has 4 years of experience as a professional audio engineer.
+              He has worked on Emmy, Grammy, and Oscar winning projects and has
+              toured in 4 continents with notable artists
+            </p>
           </div>
         </header>
       </div>
--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -1,11 +1,11 @@
 import { useNavigate } from "react-router-dom";
 import { Toolbar } from "../toolbar/Toolbar";
 import "./Home.css";
-import discordLogo from "../../assets/discord-mascot.png"
-import instalogo from "../../assets/Instagram_logo.png"
-import twittalogo from "../../assets/Twitter_logo.png"
+import discordLogo from "../../assets/discord-mascot.png";
+import instalogo from "../../assets/Instagram_logo.png";
+import twittalogo from "../../assets/Twitter_logo.png";
 import dots from "../../assets/dots.png";
-import productGIF from "../../assets/preview.gif"
+import productGIF from "../../assets/preview.gif";
 
 export function Home() {
   let navigate = useNavigate();
@@ -22,58 +22,85 @@ export function Home() {
         <header className="Home-header">
           <p className="Home-title-container"></p>
           <p className="TagLine">
-            Artist First.<br></br>Making NFT's Stupid Simple.
+            Artist First.<br></br>Making NFTs Stupid Simple.
           </p>
 
           <p className="ElevatorPitch">
-            Phoenix empowers artists to create customized NFT drops in four 
-            easy steps, and use them to build lasting relationships with fans 
-            and bring in some extra cash.<br></br>Fans can mint by uploading their 
-            ticket QR code! Easy-peasy.
+            Phoenix empowers artists to create customized NFT drops in four easy
+            steps, use them to build lasting relationships with fans, and bring
+            in some extra cash.<br></br>Fans can mint NFTs by showing us their
+            ticket! Easy-peasy.
           </p>
           <div className="row">
-          <a target="_blank" rel="noopener noreferrer" href="https://discord.gg/jh5uEccp" className="SocialButton">
-            <img src={discordLogo} className="DiscordLogo" alt="" />
-              </a>            
-          <a target="_blank" rel="noopener noreferrer" href="https://www.instagram.com/phoenix_labs_/" className="SocialButton">
-            <img src={instalogo} className="DiscordLogo" alt="" />
-              </a>            
-          <a target="_blank" rel="noopener noreferrer" href="https://twitter.com/phoenix_labs_" className="SocialButton">
-            <img src={twittalogo} className="DiscordLogo" alt="" />
-              </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://discord.gg/jh5uEccp"
+              className="SocialButton"
+            >
+              <img src={discordLogo} className="DiscordLogo" alt="" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://www.instagram.com/phoenix_labs_/"
+              className="SocialButton"
+            >
+              <img src={instalogo} className="DiscordLogo" alt="" />
+            </a>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://twitter.com/phoenix_labs_"
+              className="SocialButton"
+            >
+              <img src={twittalogo} className="DiscordLogo" alt="" />
+            </a>
           </div>
           <div className="row">
-            <p><img src={productGIF} className="ProdGIF" alt="" /></p>
+            <p>
+              <img src={productGIF} className="ProdGIF" alt="" />
+            </p>
           </div>
-          <p>Guiding Principals</p>
+          <p>Guiding Principles</p>
           <div className="row">
             <div className="MissionGoal">
-              Artist, First and Foremost<br></br> 
+              Artist, First and Foremost<br></br>
               <p className="MissonDetails">
-                We are giving power back to the content creators, not the middlemen. 
-                Phoenix is built to be simple. No knowledge of NFTs or
-                blockchains is required to utilize NFT technology.
+                We are giving power back to the content creators, not the
+                middlemen. Phoenix is built to be simple for artists. No
+                knowledge of NFTs or blockchains is required to utilize NFT
+                technology.
               </p>
             </div>
             <div className="MissionGoal">
               Tools to enhance your fanbase
               <p className="MissonDetails">
-                Rewards such early release music, signed memorabilia, or anythinig else 
-                can be distributed to fans through our platform. Artists can design their 
-                NFT collection to contain rare perks at minting or airdrop to NFT holders.
+                Rewards such early release music, signed memorabilia, and
+                anything else can be distributed to fans with our platform.
+                Artists can design their NFT collection to gift rare perks at
+                minting or airdrop the gifts to NFT holders.
               </p>
             </div>
             <div className="MissionGoal">
               Build Lasting Art
               <p className="MissonDetails">
-                In an increasingly digital world Album art, ticketing, and other lasting 
-                art is being left out of the process. We are enabling artist to create new 
-                additions to what they are already making that fans can hold onto and get perks for.
+                In an increasingly digital world album art, ticketing, and other
+                lasting art forms are being left out of the process. We are
+                enabling artist to release a new art form that fans can hold
+                onto and get perks with.
               </p>
             </div>
           </div>
           <p>
-            <br></br><br></br><br></br><br></br><br></br><br></br><br></br><br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>
             Phoenix is currently in development and will be available soon for
             artists and concert-goers
           </p>
