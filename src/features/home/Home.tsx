7f89c7f97d8a5dab1c02a42fece88247cf29334a
Jack Bloomfeld <jackbloomfeld1@gmail.com>
2022-07-17 15:15:57 -0800

    added alt props

--- src/features/home/Home.tsx
+++ src/features/home/Home.tsx
@@ -14,7 +14,6 @@ import cage from "../../assets/Cage.png";
 import help from "../../assets/GiveTech.png";
 import community from "../../assets/community.png";
 
-
 interface IHomeReduxProps {
   walletConnected: string;
 }
@@ -56,7 +55,9 @@ function Home(props: IHomeReduxProps) {
       <Toolbar isOpaque={false} />
 
       <div className="hero min-h-screen bg-gradient-to-r from-warning to-secondary">
-        <div className="hero-overlay bg-opacity-10 "><img className="sm:mt-30" src={lineArt} /></div>
+        <div className="hero-overlay bg-opacity-10 ">
+          <img className="sm:mt-30" src={lineArt} alt="lineart" />
+        </div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
             {props.walletConnected === "" && (
@@ -98,54 +99,60 @@ function Home(props: IHomeReduxProps) {
         </div>
       </div>
       <div className="h-screen min-h-screen bg-gradient-to-r from-warning to-secondary realtive snap-y snap-proximity flex grid-cols-2 w-full p-5 gap-5">
-            
         <h1
           className="text-4xl w-1/4 font-bold text-primary font-patrick text-right absolute"
           data-aos="fade-right"
           data-aos-duration="1000"
         >
-          Other than social media there is very few ways for performers to connect with fans,
+          Other than social media there is very few ways for performers to
+          connect with fans,
         </h1>
-              <img 
-              src={musician} 
-              className="h-1/2 absolute mt-72 ml-44" 
-              data-aos="flip-left"
-              data-aos-duration="1000"
-              />
-              <img 
-              src={wall} 
-              className="h-3/4 absolute mt-44 ml-44 left-96" 
-              data-aos="flip-up"
-              data-aos-duration="1000"
-              />
-              <img 
-              src={fan} 
-              className="h-1/2 absolute mt-72 mr-44 right-0" 
-              data-aos="flip-left"
-              data-aos-duration="1000"
-              />
+        <img
+          src={musician}
+          alt="musician"
+          className="h-1/2 absolute mt-72 ml-44"
+          data-aos="flip-left"
+          data-aos-duration="1000"
+        />
+        <img
+          src={wall}
+          alt="wall"
+          className="h-3/4 absolute mt-44 ml-44 left-96"
+          data-aos="flip-up"
+          data-aos-duration="1000"
+        />
+        <img
+          src={fan}
+          alt="fan"
+          className="h-1/2 absolute mt-72 mr-44 right-0"
+          data-aos="flip-left"
+          data-aos-duration="1000"
+        />
         <h1
           className="text-4xl w-1/4 font-bold text-primary font-patrick text-left right-10 mt-10 absolute"
           data-aos="fade-left"
           data-aos-duration="1000"
         >
-        build communities, and grow their fan base.
+          build communities, and grow their fan base.
         </h1>
       </div>
 
       <div className="hero min-h-screen bg-gradient-to-r from-warning to-secondary from-warning relative">
-            <img 
-              src={cage} 
-              className="h-3/4 top-10 absolute" 
-              data-aos="flip-up"
-              data-aos-duration="1000"
-              />
+        <img
+          src={cage}
+          alt="cage"
+          className="h-3/4 top-10 absolute"
+          data-aos="flip-up"
+          data-aos-duration="1000"
+        />
         <h1
           className="w-1/2 text-center text-4xl font-bold text-primary font-patrick absolute bottom-10"
           data-aos="fade-up"
           data-aos-duration="5000"
         >
-          This has forced performers to sacrifice the quality of their content to meet the latest trends so their content will be pushed to new possible fans.
+          This has forced performers to sacrifice the quality of their content
+          to meet the latest trends so their content will be pushed to new
+          possible fans.
         </h1>
       </div>
       <div className="hero min-h-screen bg-gradient-to-r from-warning to-secondary relative">
@@ -154,14 +161,17 @@ function Home(props: IHomeReduxProps) {
           data-aos="flip-up"
           data-aos-duration="5000"
         >
-          We enable performers with no technical background to build and give digital art, built on the latest technologies, to fans who attend their events. (for free!)
+          We enable performers with no technical background to build and give
+          digital art, built on the latest technologies, to fans who attend
+          their events. (for free!)
         </h1>
-        <img 
-              src={help} 
-              className="h-3/4 bottom-10 left-0 absolute" 
-              data-aos="zoom-in-up"
-              data-aos-duration="1000"
-              />
+        <img
+          src={help}
+          alt="help"
+          className="h-3/4 bottom-10 left-0 absolute"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        />
       </div>
 
       <div className="hero min-h-screen bg-gradient-to-r from-warning to-secondary relative">
@@ -170,14 +180,17 @@ function Home(props: IHomeReduxProps) {
           data-aos="flip-up"
           data-aos-duration="5000"
         >
-        This art lets fans and performers build communities based on their interests. Here they can connect with other fans, performers, and get exclusive content!
+          This art lets fans and performers build communities based on their
+          interests. Here they can connect with other fans, performers, and get
+          exclusive content!
         </h1>
-        <img 
-              src={community} 
-              className="h-3/4 bottom-10 right-0 absolute" 
-              data-aos="zoom-in"
-              data-aos-duration="1000"
-              />
+        <img
+          src={community}
+          alt="community"
+          className="h-3/4 bottom-10 right-0 absolute"
+          data-aos="zoom-in"
+          data-aos-duration="1000"
+        />
       </div>
 
       <div className="hero min-h-screen bg-gradient-to-r from-warning to-secondary relative">
@@ -186,32 +199,37 @@ function Home(props: IHomeReduxProps) {
           data-aos="flip-up"
           data-aos-duration="5000"
         >
-      Brining more money into performers pockets while bringing fans and performers closer together than ever.
+          Brining more money into performers pockets while bringing fans and
+          performers closer together than ever.
         </h1>
-        <img 
-            src={musician} 
-            className="h-1/2 bottom-10 left-0 absolute" 
-            data-aos="zoom-in-up"
-            data-aos-duration="1000"
-            />
-          <img 
-            src={fan} 
-            className="h-1/2 bottom-10 left-15 absolute" 
-            data-aos="fade-left"
-            data-aos-duration="1000"
-            />
-          <img 
-            src={fan} 
-            className="h-1/2 bottom-10 left-15 absolute" 
-            data-aos="zoom-in-up"
-            data-aos-duration="1000"
-            />
-          <img 
-            src={fan} 
-            className="h-1/2 bottom-10 left-15 absolute" 
-            data-aos="zoom-in-up"
-            data-aos-duration="1000"
-            />
+        <img
+          src={musician}
+          alt="musician"
+          className="h-1/2 bottom-10 left-0 absolute"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        />
+        <img
+          src={fan}
+          alt="fan1"
+          className="h-1/2 bottom-10 left-15 absolute"
+          data-aos="fade-left"
+          data-aos-duration="1000"
+        />
+        <img
+          src={fan}
+          alt="fan2"
+          className="h-1/2 bottom-10 left-15 absolute"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        />
+        <img
+          src={fan}
+          alt="fan3"
+          className="h-1/2 bottom-10 left-15 absolute"
+          data-aos="zoom-in-up"
+          data-aos-duration="1000"
+        />
       </div>
 
       <Footer />
