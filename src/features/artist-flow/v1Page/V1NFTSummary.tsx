2bb998f4e2ca98234923f7b4fe25d3d19e6769ac
troynohra <troy.nohra@gmail.com>
2022-07-18 18:39:44 -0700

    Pause for Demo

--- src/features/artist-flow/v1Page/V1NFTSummary.tsx
+++ src/features/artist-flow/v1Page/V1NFTSummary.tsx
@@ -14,17 +14,34 @@ function V1NFTSummary(props: IV1NFTSummaryProps) {
         <h2 className="card-title font-xl font-patrick text-secondary">
           NFT Summary
         </h2>
-        <div className="">
-          <p className="font-xl font-patrick text-neutral">{props.nftTitle}</p>
+        <div className="grid grid-rows-1 m-2">
+          <div className="grid grid-cols-4">
+            <div className="cols-span-1">
+            <p className="font-sm font-patrick text-secondary">Title: </p>
+            <p className="font-xl font-patrick text-neutral">{props.nftTitle}</p>
+            </div>
+            <div className="cols-span-1">
+            <p className="font-sm font-patrick text-secondary">Grouping: </p>
+            <p className="font-xl font-patrick text-neutral">{props.nftGroup}</p>
+            </div>
+            <div className="cols-span-1">
+            <p className="font-sm font-patrick text-secondary">How many: </p>
+            <p className="font-xl font-patrick text-neutral">Jack to put data</p>
+            </div>
+            <div className="cols-span-1">
+            <p className="font-sm font-patrick text-secondary">Ownership: </p>
+            <p className="font-xl font-patrick text-neutral">Jack to put data</p>
+            </div>
           <div>
-            <div className="card-actions">
+            <div className="card-actions m-2">
               <label title={props.nftTitle}>
-                <p className="btn btn-primary m-3">Select File</p>
                 <div>
                   <img className="preview" src={props.nftImagePreview} alt="" />
                 </div>
+                <p className="btn btn-primary m-3">Select File</p>
               </label>
             </div>
+          </div>
           </div>
         </div>
       </div>
